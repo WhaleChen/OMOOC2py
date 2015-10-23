e1be0a15d7a37eaceb78c954f3dadcc0acab36aa
WhaleChen <18621050278@163.com>
2015-10-24 01:23:58 +0800

    Create modulefromZoomq.py

---
+++ modulefromZoomq.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+#!/user/bin/env python
+#头部声明
+···文件说明
+作者信息
+版本自述
+...
+···
+#全局引用
+import sys
+#全局变量
+PATH = "path/2/work dir"
+#函数撰写
+def foo():
+  pass
+  return none
+
+#自检区
+if __name__=='_main_':
+  if 1!= len(sys.argv):
+    print ```Usage:
+  $ pyhon main...py
+    ```
+    else:
+      foo()
